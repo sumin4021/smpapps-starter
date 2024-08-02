10daf0eded3924d88f087988156d00b18aa7eebb
박수민 <sumin4021@gmail.com>
2024-08-02 15:56:11 +0900

    insta

---
+++ src/main/java/com/smpapps/starter/security/OAuth2_InstagramUserInfo.java
@@ -0,0 +1,36 @@
+package com.smpapps.starter.security;
+
+import java.util.Map;
+
+import lombok.Getter;
+
+@Getter
+public class OAuth2_InstagramUserInfo implements OAuth2_UserInfo {
+
+  private Map<String, Object> attributes;
+
+  public OAuth2_InstagramUserInfo(Map<String, Object> attributes) {
+    this.attributes = attributes;
+  }
+
+  @Override
+  public String getId() {
+    return (String) attributes.get("id");
+  }
+
+  @Override
+  public String getName() {
+    return (String) attributes.get("username");
+  }
+
+  @Override
+  public String getEmail() {
+    return (String) attributes.get("username");
+  }
+
+  @Override
+  public String getProfileImage() {
+    return "";
+  }
+
+}
